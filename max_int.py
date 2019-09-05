7f5898155345b36e2dae5e1d401c23c365d9526b
hjalti <hjalti19@ru.is>
2019-09-05 10:42:51 +0000

    code clear and easily read

--- max_int.py
+++ max_int.py
@@ -1,9 +1,9 @@
 #búum til input sem endurtekur sig (notandi getur sett inn gildi afur og aftur)
 #Látum forritið stöðvast þegar sett er inn neikvæð tala
 #Fáum forritið til að halda utan um stærsta jákvæða gildið og prenta það út þegar forritið stöðvast
-Max = 0
+Max_int = 0
 while True:
     num_int = int(input("Input number: "))
-    if num_int > Max:
-        Max = num_int
-print(Max)
+    if num_int > Max_int:
+        Max_int = num_int
+print(Max_int)
