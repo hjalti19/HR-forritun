705429a7b152b0370a8dc42658f7d7c5b9de0478
hjalti <hjalti19@ru.is>
2019-09-05 10:00:09 +0000

    Algorithim written for max_int

---
+++ max_int.py
@@ -0,0 +1,3 @@
+#búum til input sem endurtekur sig (notandi getur sett inn gildi afur og aftur)
+#Látum forritið stöðvast þegar sett er inn neikvæð tala
+#Fáum forritið til að halda utan um stærsta jákvæða gildið
